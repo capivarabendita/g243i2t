2c43bd8ac2206fdff883d509a313c0d8b5aa255a
sujiro kimi mami <127323578+capivaramaldita@users.noreply.github.com>
2024-06-10 08:58:58 -0300

    Add files via upload

---
+++ script/login.js
@@ -0,0 +1,26 @@
+const btnlogin = document.querySelector("#btnlogin");
+const inputUsuario = document.querySelector("#usuario");
+const inputsenha = document.querySelector("#senha");
+let usuariologado = null;
+
+(()=> {
+    usuariologado = localStorage.getItem("usuario");
+    if(usuariologado){
+        window.location.href = "/tmp/guest-flg3kk/Downloads/g243i2t-main/logado.html";
+
+    }})();
+
+    btnlogin.onclick = (e) =>{
+        e.preventDefault();
+        let usuario = inputUsuario.value;
+        let senha = inputsenha.value;
+        if(usuario){
+            if(usuario ==="cabranoiada"){
+                if(senha === "123"){
+                    window.location.href = "/tmp/guest-flg3kk/Downloads/g243i2t-main/logado.html";
+                }
+            }else{
+                inputUsuario.focus();
+            }
+        }
+    }
